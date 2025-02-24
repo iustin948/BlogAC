58c01bc0f624955a2d86c3ada2c788e11fb5a1b5
iustin948 <nicolaie-iustin.homoranu@student.tuiasi.ro>
2025-02-24 14:31:53 +0200

    angular 19 + taiga + tailwind

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
